7ab5c87f5c6a4f6f2675f86fa5a408c4f7406ae6
winson65 <winson.yu22@gmail.com>
2021-02-26 18:13:33 -0500

    initial config

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 // Connect to FireBase
 var admin = require("firebase-admin");
 
-var serviceAccount = require("serviceAccountKey.json");
+var serviceAccount = require("./service-account.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -15,3 +15,6 @@ admin.initializeApp({
 
 
 app.listen(5000, () => console.log('App is listening on Port 5000'));
+app.get('/', function (req, res) {
+  res.send('hello world')
+})
