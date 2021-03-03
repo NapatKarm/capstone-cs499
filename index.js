6235385643774c948988b23d1740a0e98b929aa4
Brian Guo <38799617+IZenithI@users.noreply.github.com>
2021-03-02 21:55:46 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,54 @@
+// require('dotenv').config();
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Connect to FireBase
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./service-account.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://c-vivid-default-rtdb.firebaseio.com"
+});
+
+app.get('/', function (req, res) {
+  res.send('hello world')
+});
+
+//Declare Test/Temp Map
+let authMap = new Map();
+
+app.post('/signup', async (req, res) => {
+  if(authMap.has(req.body.email)){
+    res.status(422).send("Username already in use");
+  }
+  else{
+    authMap.set(req.body.email, req.body.password);
+    res.sendStatus(200);
+  }
+});
+
+app.post('/login', async (req, res) => {
+  if(authMap.has(req.body.email)){
+    if(authMap.get(req.body.email) == req.body.password){
+      res.sendStatus(200).send("Login Success");
+      return;
+    }
+    else{
+      res.sendStatus(401).send("Incorrect Password");
+      return;
+    }
+  }
+  else{
+    res.sendStatus(404).send("Username Not Found");
+    return;
+  }
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`App is listening on Port ${port}`));
