a7444ac4f13e50dea6415afc09ded11c5129eaf3
winson65 <winson.yu22@gmail.com>
2021-03-04 00:07:57 -0500

    half done with businessjoin

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Connect to FireBase
-var admin = require("firebase-admin");
-var serviceAccount = require("./service-account.json");
+var admin = require('firebase-admin');
+var serviceAccount = require('./service-account.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://c-vivid-default-rtdb.firebaseio.com"
+  databaseURL: 'https://c-vivid-default-rtdb.firebaseio.com'
 });
 
 const db = admin.firestore();
@@ -33,7 +33,7 @@ let businessMap = new Map();
 app.post('/signup', async (req, res) => {  //Expected request: {firstname, lastname, email, password}
   const existing_user = usersdb.where('email', '==', req.body.email).get();
   if(!existing_user.empty) {               //Email is taken
-    res.status(422).send("Username already in use");
+    res.status(422).send('Username already in use');
   }
   else{
     const userInfo = {                     //User Info Structure
@@ -45,7 +45,7 @@ app.post('/signup', async (req, res) => {  //Expected request: {firstname, lastn
     };
     try {
       await usersdb.add(userInfo);
-      res.status(200).send("Successfully registered");
+      res.status(200).send('Successfully registered');
     } catch(error){
       console.log(error);
     }
@@ -58,7 +58,7 @@ app.post('/signin', async (req, res) => {         //Expected request: {email, pa
   // For security reasons, you do not disclose whether email or password is invalid
   try {
     if(!existing_user.empty || existing_user.get('email') != req.body.email || existing_user.get('password') !=  req.body.password) {  // Compares username and passwords to queried document
-      res.status(422).send("Invalid email or password");
+      res.status(422).send('Invalid email or password');
     }
     else {
       res.status(200).send(existing_user.data());
@@ -68,67 +68,53 @@ app.post('/signin', async (req, res) => {         //Expected request: {email, pa
   }
 });
 
-app.post('/businessRegister', async (req, res) => {     //Expected request: { businessname, businessaddr, owner, businesspass} (owner: email?)
+app.put('/businessRegister', async (req, res) => {     //Expected request: { businessname, businessaddr, owner, businesspass, first name, last name} (owner: email?)
   const existing_business = usersdb.where('businessaddr', '==', req.body.businessaddr).get();
-  if(!existing_business){   //Business already registered, cannot have 2 businesses on same address
-    res.status(400).send("Business Already Registered")
+  if(!existing_business) {   //Business already registered, cannot have 2 businesses on same address
+    res.status(400).send('Business Already Registered');
   }
-  else{
-    const businessInfo = {                                    //Business Info Structure
-      "businessname": req.body.businessname,
-      "businessaddr": req.body.businessaddr,
-      "businesspass": req.body.businesspass,
-      "members": [{                                     //Member Info Structure
-        "firstname": authMap.get(req.body.owner).firstname,
-        "lastname": authMap.get(req.body.owner).lastname,
-        "email": req.body.owner,
-        "role": "admin"
+  else {
+    const businessInfo = {                       //Business Info Structure
+      'businessname': req.body.businessname,
+      'businessaddr': req.body.businessaddr,
+      'businesspass': req.body.businesspass,
+      'members': [{                              //Member Info Structure
+        'firstname': req.body.firstname,
+        'lastname': req.body.lastname,
+        'email': req.body.owner,
+        'role': 'admin'
       }]
     };
     try {
-    await busdb.add(businessInfo);        //Add business data to business 'database'
+      await busdb.add(businessInfo);       
     } catch(error) {
       console.log(error);
     }
   }
 });
 
-app.post('/businessJoin', async (req, res) => {                     //Expected request: {email, businesspass, businessaddr, role}
-  const existing_business = usersdb.where('businessaddr', '==', req.body.businessaddr).get();
-  if(!existing_business){   // Non-existing Business
-    res.status(400).send("Business Already Registered")
+app.post('/businessJoin', async (req, res) => {                     //Expected request: {email, businesspass, businessaddr}
+  const existing_business = usersdb.where('businesspass', '==', req.body.businesspass).get();
+  if (!existing_business) {                                       // Non-existing Business, false = empty document
+    res.status(400).send('Business does not exist');
+  }
+  for (let i = 0; i < existing_business.members.length; i++) {  // Check if member already exists in a business
+    if (existing_business.members[i].email == req.body.email) {
+      res.status(422).send('User already is a member');
+    }
+  }                                                                                                                
+  const new_member = ({                                   //Add user as member under the business
+    'firstname': req.body.firstname,
+    'lastname': req.body.lastname,
+    'email': req.body.email,
+    'role': 'employee'
+  });
+  try {
+    res.status(200).send('Successfully Joined'); 
+  } catch (error) {
+    console.log(error);
   }
   
-  else if(req.body.passcode != businessMap.get(req.body.businessaddr).businesspass){        //If incorrect passcode
-    res.status(400).send("Incorrect Passcode");
-  }
-  else{                                                             //If existing business and correct passcode
-    businessInfo = businessMap.get(req.body.businessaddr);          //Get data from business 'database'
-    userInfo = authMap.get(req.body.email);                         //Get data from user 'database'
-
-    alreadyInBusiness = false;
-    for(i = 0; i < businessInfo.members.length; i++){               //Check if user is already registered under the business
-      if(userInfo.email == businessInfo.members[i].email){
-        alreadyInBusiness = true;
-        break;
-      }
-    }
-
-    if(alreadyInBusiness == true){                                  //If user has already been registered
-      res.status(400).send("You have already been registered")
-    }
-    else{                                                           //If user has not been registered
-      businessInfo.members.push({                                   //Add user as member under the business
-        "firstname": userInfo.firstname,
-        "lastname": userInfo.lastname,
-        "email": req.body.email,
-        "role": req.body.role
-      });
-      userInfo.businesses.push(businessInfo);                       //Add business data to user 'database' under businesses
-
-      res.status(200).send("Successfully Joined");
-    }
-  }
 });
 
 const port = process.env.PORT || 5000;
