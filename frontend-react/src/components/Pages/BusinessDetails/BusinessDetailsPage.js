f36f6553ba3e72ec261d668cb8c2b6da5d4d9204
NapatKarm <napat.karm@gmail.com>
2021-03-10 20:42:11 -0500

    Debugging of Promote/Demote/Kick

--- frontend-react/src/components/Pages/BusinessDetails/BusinessDetailsPage.js
+++ frontend-react/src/components/Pages/BusinessDetails/BusinessDetailsPage.js
@@ -10,14 +10,24 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import axios from 'axios';
-
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import RefreshIcon from '@material-ui/icons/Refresh';
+import IconButton from '@material-ui/core/IconButton';
 
 class BusinessDetailsPage extends Component {
     constructor(props) {
         super(props);
         this.state = {
             businessDetails: undefined,
-            role: undefined
+            role: undefined,
+            action: false,
+            actionName: "",
+            actionVictim: ""
         }
     }
     componentDidMount() {
@@ -26,29 +36,138 @@ class BusinessDetailsPage extends Component {
         }, () => {
             console.log("Updated Business Details", this.state.businessDetails)
             let roleGrab = this.state.businessDetails.members.find(element => element.email == this.props.userData.email)
+            if(roleGrab!==undefined) {
+                this.setState({
+                    role: roleGrab.role
+                })
+            }
+            else alert("Something is wrong, you are not in this business but you can still see it.")
+
+        })
+    }
+    updateDetails = async () => {
+        console.log("UPDATING")
+        axios.post(`https://c-vivid-backend.herokuapp.com/getSingleBusinessData`, {
+            business_id: this.state.businessDetails.business_id,
+            email: this.props.userData.email,
+            token: this.props.userData.token
+        })
+        .then(res => {
             this.setState({
-                role: roleGrab.role
+                businessDetails: res.data,
+                action: false
             })
+            let roleGrab = this.state.businessDetails.members.find(element => element.email == this.props.userData.email)
+            if(roleGrab!==undefined) {
+                this.setState({
+                    role: roleGrab.role
+                })
+            }
+            else {
+                alert("You have been removed from the business")
+                this.props.history.push("/home");
+            }
+        })
+        .catch(err => {
+            console.log("Error from UPDATE",err)
+            
+        })
+    }
+    cancelAction = () => {
+        this.setState({
+            action: false
         })
     }
     runPromote = (changeeEmail) => {
         console.log("Promoting")
+        this.setState({
+            actionName: "promote",
+            actionVictim: changeeEmail,
+            action:true
+        })
     }
     runDemote = (changeeEmail) => {
         console.log("Demoting")
+        this.setState({
+            actionName: "demote",
+            actionVictim: changeeEmail,
+            action:true
+        })
     }
     runKick = (kickeeEmail) => {
         console.log("Kicking")
+        this.setState({
+            actionName: "kick",
+            actionVictim: kickeeEmail,
+            action:true
+        })
     }
-    goBackHome =()=> {
+    goBackHome = () => {
         this.props.bClear()
         this.props.history.push("/home")
+    }
+    confirmAction = async () => {
+        if(this.state.actionName==="promote") {
+            await axios.put(`https://c-vivid-backend.herokuapp.com/roleChange`, {
+                business_id: this.state.businessDetails.business_id,
+                changerEmail: this.props.userData.email,
+                changeeEmail: this.state.actionVictim,
+                newRole: "Admin",
+                token: this.props.userData.token
+            })
+            .then(res => {
+                console.log("Response from PROMOTE",res);
+                this.updateDetails();
+            })
+            .catch(err => {
+                console.log("Error from PROMOTE",err)
+                
+            })
+        }
+        else if(this.state.actionName==="demote") {
+            await axios.put(`https://c-vivid-backend.herokuapp.com/roleChange`, {
+                business_id: this.state.businessDetails.business_id,
+                changerEmail: this.props.userData.email,
+                changeeEmail: this.state.actionVictim,
+                newRole: "Employee",
+                token: this.props.userData.token
+            })
+            .then(res => {
+                console.log("Response from DEMOTE",res);
+                this.updateDetails();
+
+            })
+            .catch(err => {
+                console.log("Error from DEMOTE",err)
+                
+            })
+        }
+        else if(this.state.actionName==="kick") {
+            await axios.put(`https://c-vivid-backend.herokuapp.com/kickMember`, {
+                business_id: this.state.businessDetails.business_id,
+                kickerEmail: this.props.userData.email,
+                kickeeEmail: this.state.actionVictim,
+                token: this.props.userData.token
+            })
+            .then(res => {
+                console.log("Response from KICK",res);
+                this.updateDetails();
+            })
+            .catch(err => {
+                console.log("Error from KICK",err)
+                
+            })
+        }
+        else console.log("No Action Set")
     }
     render() {
         return (
             <div>
                 <div>
                     <Button onClick={this.goBackHome}>Back Home</Button>
+                    <IconButton aria-label="refresh" onClick={()=>this.updateDetails()}>
+                                <RefreshIcon />
+                    </IconButton>
                 </div>
                 <div>Your Role: {this.state.role}</div>
                 {this.state.businessDetails ? (
@@ -80,45 +199,53 @@ class BusinessDetailsPage extends Component {
                                                 <TableCell align="right">{member.lastname}</TableCell>
                                                 <TableCell align="right">{member.email}</TableCell>
                                                 <TableCell align="right">{member.role}</TableCell>
-                                                {(((this.state.role==="Owner")|(this.state.role==="Admin"))&&(this.props.userData.email!==member.email)) ? 
-                                                (
-                                                    this.state.role==="Owner" ? 
+                                                {(((this.state.role === "Owner") | (this.state.role === "Admin")) && (this.props.userData.email !== member.email)) ?
                                                     (
-                                                        member.role==="Admin" ? 
-                                                        (
-                                                            <TableCell>
-                                                            <Button color="primary" onClick={()=>this.runPromote(member.email)} disabled>Promote</Button>
-                                                            <Button color="primary" onClick={()=>this.runDemote(member.email)}>Demote</Button>
-                                                            <Button color="primary" onClick={()=>this.runKick(member.email)}>Kick</Button>
-                                                        </TableCell>
-                                                        ):(
-                                                            <TableCell>
-                                                            <Button color="primary" onClick={()=>this.runPromote(member.email)}>Promote</Button>
-                                                            <Button color="primary" onClick={()=>this.runDemote(member.email)} disabled>Demote</Button>
-                                                            <Button color="primary" onClick={()=>this.runKick(member.email)}>Kick</Button>
-                                                        </TableCell>
-                                                        )
+                                                        this.state.role === "Owner" ?
+                                                            (
+                                                                member.role === "Admin" ?
+                                                                    (
+                                                                        <TableCell align="center">
+                                                                            <ButtonGroup color="primary" aria-label="outlined primary button group">
+                                                                                <Button onClick={() => this.runPromote(member.email)} disabled>Promote</Button>
+                                                                                <Button onClick={() => this.runDemote(member.email)}>Demote</Button>
+                                                                                <Button onClick={() => this.runKick(member.email)}>Kick</Button>
+                                                                            </ButtonGroup>
+                                                                        </TableCell>
+                                                                    ) : (
+                                                                        <TableCell align="center">
+                                                                            <ButtonGroup color="primary" aria-label="outlined primary button group">
+                                                                                <Button  onClick={() => this.runPromote(member.email)}>Promote</Button>
+                                                                                <Button  onClick={() => this.runDemote(member.email)} disabled>Demote</Button>
+                                                                                <Button  onClick={() => this.runKick(member.email)}>Kick</Button>
+                                                                            </ButtonGroup>
+                                                                        </TableCell>
+                                                                    )
 
-                                                    ):(
-                                                        ((member.role==="Admin")|(member.role==="Owner")) ? 
-                                                        (
-                                                            <TableCell>
-                                                            <Button color="primary" onClick={()=>this.runPromote(member.email)} disabled>Promote</Button>
-                                                            <Button color="primary" onClick={()=>this.runDemote(member.email)} disabled>Demote</Button>
-                                                            <Button color="primary" onClick={()=>this.runKick(member.email)} disabled>Kick</Button>
-                                                        </TableCell>
-                                                        ):(
-                                                            <TableCell>
-                                                            <Button color="primary" onClick={()=>this.runPromote(member.email)} disabled>Promote</Button>
-                                                            <Button color="primary" onClick={()=>this.runDemote(member.email)} disabled>Demote</Button>
-                                                            <Button color="primary" onClick={()=>this.runKick(member.email)}>Kick</Button>
-                                                        </TableCell>
-                                                        )                                                     
-                                                    )
-                                                    
-                                                ):(
-                                                    <TableCell></TableCell>
-                                                )}
+                                                            ) : (
+                                                                ((member.role === "Admin") | (member.role === "Owner")) ?
+                                                                    (
+                                                                        <TableCell align="center">
+                                                                            <ButtonGroup color="primary" aria-label="outlined primary button group">
+                                                                                <Button onClick={() => this.runPromote(member.email)} disabled>Promote</Button>
+                                                                                <Button onClick={() => this.runDemote(member.email)} disabled>Demote</Button>
+                                                                                <Button onClick={() => this.runKick(member.email)} disabled>Kick</Button>
+                                                                            </ButtonGroup>
+                                                                        </TableCell>
+                                                                    ) : (
+                                                                        <TableCell align="center">
+                                                                            <ButtonGroup color="primary" aria-label="outlined primary button group">
+                                                                                <Button onClick={() => this.runPromote(member.email)} disabled>Promote</Button>
+                                                                                <Button onClick={() => this.runDemote(member.email)} disabled>Demote</Button>
+                                                                                <Button onClick={() => this.runKick(member.email)}>Kick</Button>
+                                                                            </ButtonGroup>
+                                                                        </TableCell>
+                                                                    )
+                                                            )
+
+                                                    ) : (
+                                                        <TableCell></TableCell>
+                                                    )}
                                             </TableRow>
                                         ))}
                                     </TableBody>
@@ -132,6 +259,24 @@ class BusinessDetailsPage extends Component {
                         or you know.. you did something you weren't supposed to
                     </div>
                 )}
+            <div>
+            <Dialog open={this.state.action} onClose={this.cancelAction} aria-labelledby="form-dialog-title">
+                            <DialogTitle id="form-dialog-title">Warning</DialogTitle>
+                            <DialogContent>
+                                <DialogContentText>
+                                    Are you sure you want to {this.state.actionName} {this.state.actionVictim}?
+                                 </DialogContentText>
+                            </DialogContent>
+                            <DialogActions>
+                                <Button onClick={this.cancelAction} color="primary">
+                                    Cancel
+                                </Button>
+                                <Button onClick={this.confirmAction} color="primary">
+                                    Confirm
+                                </Button>
+                            </DialogActions>
+                        </Dialog>
+            </div>
             </div>
         )
     }
