10fd1f96b7f44f6d28c05806e528370cf8814b9b
Jackie He <Jackie.He8@gmail.com>
2021-03-12 17:54:15 -0500

    Frontend UI/UX Update
    LandingPage left side mostly complete, working on LoginPage component

--- frontend-react/src/components/Pages/Landing/LandingPage.js
+++ frontend-react/src/components/Pages/Landing/LandingPage.js
@@ -29,25 +29,28 @@ class LandingPage extends Component {
                     <div className="content">
                         <div className="leftSide">
                             <img className="landingLogo" src={longLogo} alt="C-Vivid"/>
-                            <h2>Welcome to C-Vivid</h2>
-                            <p>Some two line preech about nothing at all in general - lorum ipsum dolar sit amet, consectetur adipiscing elit. Proin phareta lorem aba aba aba</p>
+                            <div className="titleHeader">
+                                Welcome to C-Vivid
+                            </div>
+                            <div className="aboutP">
+                                <p>Some two line preech about nothing at all in general - lorum ipsum dolar sit amet, consectetur adipiscing elit. Proin phareta lorem aba aba aba</p>
+                            </div>
 
                             {/* vvv Link page vvv*/}
                             {/* ()=>  -- stop stuff from auto running*/}
-                            <div className="mapButton">
-                                <Button>GO TO THE MAP</Button>
-                            </div>
+                            <Button style={{color: 'white', backgroundColor: '#e8333a', maxWidth: '325px', maxHeight: '50px', minWidth: '325px', minHeight: '50px'}}>GO TO THE MAP</Button>
+                            <p><br/>Businesses/Workers:</p>
                             <div className="actionButton">
-                                <Button onClick={()=>this.setState({rightState: "register"})}>REGISTER</Button>
-                                <Button onClick={()=>this.setState({rightState: "login"})}>LOGIN</Button>
+                                <Button onClick={()=>this.setState({rightState: "register"})} style={{color: 'black', backgroundColor: 'white', maxWidth: '160px', maxHeight: '50px', minWidth: '160px', minHeight: '50px'}}>REGISTER</Button>
+                                <Button onClick={()=>this.setState({rightState: "login"})}  style={{color: 'white', backgroundColor: '#b71c1c', maxWidth: '160px', maxHeight: '50px', minWidth: '160px', minHeight: '50px'}}>LOGIN</Button>
                             </div>
                         </div>
                         <div className="rightSide">
                             {this.state.rightState==="none" ? ("") : (<div>{this.state.rightState==="register" ? (<RegisterTable signUpError={this.props.signUpError} userSignup={this.props.userSignup} signupResult={this.props.signupResult}/>) : (<LoginTable logInError={this.props.logInError} userLogin={this.props.userLogin}/>)}</div>)}
                         </div>
                     </div>
-                    <footer>
-                        <p>Our names</p>
+                    <footer className="footerLanding">
+                        <p className="footerText">Hunter College Capstone Project by <a id="gitLink" href="https://github.com/IZenithI">Brian Guo</a>, <a id="gitLink" href="https://github.com/Jhe0031">Jia Qi He</a>, <a id="gitLink" href="https://www.linkedin.com/in/napatkarm/">Napat Karmniyanont</a>, and <a id="gitLink" href="https://github.com/winson65">Winson Yu</a></p>
                     </footer>
                 </div>
             </div>
